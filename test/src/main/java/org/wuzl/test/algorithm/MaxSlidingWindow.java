e492becaea0af284d7b2d127b0bc2370589b25fc
ziliang.wu <ziliang.wu@lvyuetravel.com>
2020-03-04 16:16:19 +0800

    add

--- test/src/main/java/org/wuzl/test/algorithm/MaxSlidingWindow.java
+++ test/src/main/java/org/wuzl/test/algorithm/MaxSlidingWindow.java
@@ -2,6 +2,7 @@ package org.wuzl.test.algorithm;
 
 import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
 /**
  * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
@@ -15,10 +16,11 @@ import java.util.Arrays;
  */
 public class MaxSlidingWindow {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(maxSlidingWindow(new int[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3)));
+        MaxSlidingWindow obj = new MaxSlidingWindow();
+        System.out.println(Arrays.toString(obj.maxSlidingWindow(new int[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3)));
     }
 
-    public static int[] maxSlidingWindow(int[] nums, int k) {
+    public int[] maxSlidingWindow(int[] nums, int k) {
         if (nums == null || nums.length < k) {
             return null;
         }
--- test/src/main/java/org/wuzl/test/algorithm/TestSort.java
+++ test/src/main/java/org/wuzl/test/algorithm/TestSort.java
@@ -11,87 +11,144 @@ import java.util.List;
  *
  */
 public class TestSort {
-	private static final Integer[] baseArray = { 81, 94, 11, 96, 12, 35, 17, 95, 28, 58, 41, 75, 15 };
+    // private static final int[] baseArray = { 81, 94, 11, 96, 12, 35, 17, 95, 28, 58, 41, 75, 15 };
+    private static final int[] baseArray = { 81, 94, 11, 96, 12, 35, 17, 95, 28, 58 };
 
-	public static void main(String[] args) {
-		System.out.println("输入:" + Arrays.toString(baseArray));
-		// outArray(shellSort(baseArray.clone()));
-		List<Integer> list = new ArrayList<>(Arrays.asList(baseArray));
-		quickSort(list);
-		System.out.println(list);
+    public static void main(String[] args) {
+        System.out.println("输入:" + Arrays.toString(baseArray));
+        // outArray(shellSort(baseArray.clone()));
+        // List<Integer> list = new ArrayList<>(Arrays.asList(baseArray));
+        quickSort(baseArray);
+        System.out.println(Arrays.toString(baseArray));
 
-	}
+    }
 
-	private static void outArray(int[] array) {
-		System.out.println(Arrays.toString(array));
-	}
+    private static void outArray(int[] array) {
+        System.out.println(Arrays.toString(array));
+    }
 
-	/**
-	 * 插入排序
-	 * 
-	 * @param array
-	 * @return
-	 */
-	public static int[] insertionSort(int[] array) {
-		int j;
-		for (int p = 1; p < array.length; p++) {
-			int temp = array[p];
-			// 没进循环的话j--失效
-			for (j = p; j > 0 && temp < array[j - 1]; j--) {
-				array[j] = array[j - 1];
-			}
-			array[j] = temp;
-		}
-		return array;
-	}
+    /**
+     * 插入排序
+     * 
+     * @param array
+     * @return
+     */
+    public static int[] insertionSort(int[] array) {
+        int j;
+        for (int p = 1; p < array.length; p++) {
+            int temp = array[p];
+            // 没进循环的话j--失效
+            for (j = p; j > 0 && temp < array[j - 1]; j--) {
+                array[j] = array[j - 1];
+            }
+            array[j] = temp;
+        }
+        return array;
+    }
 
-	/**
-	 * 希尔排序
-	 * 
-	 * @param array
-	 * @return
-	 */
-	public static int[] shellSort(int[] array) {
-		int j;
-		for (int gap = array.length; gap > 0; gap = gap / 2) {
-			for (int i = gap; i < array.length; i++) {
-				int temp = array[i];
-				for (j = i; j >= gap && temp < array[j - gap]; j = j - gap) {
-					array[j] = array[j - gap];
-				}
-				array[j] = temp;
-			}
-		}
-		return array;
-	}
+    /**
+     * 希尔排序
+     * 
+     * @param array
+     * @return
+     */
+    public static int[] shellSort(int[] array) {
+        int j;
+        for (int gap = array.length; gap > 0; gap = gap / 2) {
+            for (int i = gap; i < array.length; i++) {
+                int temp = array[i];
+                for (j = i; j >= gap && temp < array[j - gap]; j = j - gap) {
+                    array[j] = array[j - gap];
+                }
+                array[j] = temp;
+            }
+        }
+        return array;
+    }
 
-	/**
-	 * 快速排序
-	 * 
-	 * @param rows
-	 */
-	public static void quickSort(List<Integer> rows) {
-		if (rows.size() <= 1) {
-			return;
-		}
-		List<Integer> smaller = new ArrayList<Integer>();
-		List<Integer> same = new ArrayList<>();
-		List<Integer> lager = new ArrayList<>();
-		int choseItem = rows.get(rows.size() / 2);
-		for (Integer item : rows) {
-			if (item < choseItem) {
-				smaller.add(item);
-			} else if (item > choseItem) {
-				lager.add(item);
-			} else {
-				same.add(item);
-			}
-		}
-		quickSort(smaller);
-		quickSort(lager);
-		rows.clear();
-		rows.addAll(smaller);
-		rows.addAll(same);
-		rows.addAll(lager);
-	}
+    /**
+     * 有时间再写吧 心乱
+     * 
+     * @param array
+     */
+    public static void quickSort(int[] array) {
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int i, int j) {
+        if (i >= j) {
+            return;
+        }
+        if (i + 1 == j) {
+            if (array[i] > array[j]) {
+                swap(array, i, j);
+            }
+            return;
+        }
+        int value = array[i];
+        // int tempIndex = i;
+        // if (value < array[(j - i) / 2]) {
+        // tempIndex = (j - i) / 2;
+        // }
+        // if (value < array[j]) {
+        // tempIndex = j;
+        // }
+        // if (i != tempIndex) {
+        // swap(array, i, tempIndex);
+        // }
+        int left = i + 1;
+        int right = j;
+        while (left <= right) {
+            while (array[right] > value) {
+                right--;
+            }
+            while (array[left] < value) {
+                left++;
+            }
+            if (left == right) {
+                swap(array, left, i);
+            } else {
+                swap(array, left, right);
+            }
+        }
+        System.out.println(Arrays.toString(array));
+        // quickSort(array, i, left);
+        // quickSort(array, left, j);
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    /**
+     * 快速排序 这种有问题呀 ???大量的list
+     * 
+     * @param rows
+     */
+    public static void quickSortV1(List<Integer> rows) {
+        if (rows.size() <= 1) {
+            return;
+        }
+        List<Integer> smaller = new ArrayList<Integer>();
+        List<Integer> same = new ArrayList<>();
+        List<Integer> lager = new ArrayList<>();
+        int choseItem = rows.get(rows.size() / 2);
+        for (Integer item : rows) {
+            if (item < choseItem) {
+                smaller.add(item);
+            } else if (item > choseItem) {
+                lager.add(item);
+            } else {
+                same.add(item);
+            }
+        }
+        quickSortV1(smaller);
+        quickSortV1(lager);
+        rows.clear();
+        rows.addAll(smaller);
+        rows.addAll(same);
+        rows.addAll(lager);
+    }
 }
